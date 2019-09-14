445582fcff1626e14627b1ab4d489069ac074a12
Katie Williams <kawilliams8@gmail.com>
2019-09-13 22:23:59 -0600

    Add snapshot test for Park

---
+++ src/components/Park/Park.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Park from '../Park/Park';
+
+describe('Park', () => {
+
+  let wrapper, mockPark;
+  beforeEach(() => {
+    mockPark = {
+      "states": "DC",
+      "directionsInfo": "The memorial is located at the corner of...",
+      "directionsUrl": "http://www.nps.gov/afam/planyourvisit/directions.htm",
+      "url": "https://www.nps.gov/afam/index.htm",
+      "weatherInfo": "Washington DC gets to see all four seasons...",
+      "name": "African American Civil War Memorial",
+      "latLong": "lat:38.916554, long:-77.025977",
+      "description": "Over 200,000 African-American soldiers...",
+      "designation": "",
+      "parkCode": "afam",
+      "id": "1A47416F-DAA3-4137-9F30-14AF86B4E547",
+      "fullName": "African American Civil War Memorial",
+      "type": "parks",
+      "images": [
+        {
+          "credit": "NPS Photo",
+          "altText": "Site Statue",
+          "title": "African American Civil War Statue",
+          "id": "3406",
+          "caption": "A poignant reminder of our nations past",
+          "url": "https://www.nps.gov/common/uploads/structured_data/3C816B50-1DD8-B71B-0BF380049FB6B6A2.jpg"
+        }
+      ]
+    }
+    wrapper = shallow(<Park park={mockPark}/>)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+})
