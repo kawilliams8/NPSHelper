eb772fdb31347d78d7ede56aba1853f4ed06a567
Katie Williams <kawilliams8@gmail.com>
2019-09-16 20:51:42 -0600

    Style 404 text and image, update copy

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -5,9 +5,9 @@ import Arrowhead from '../../assets/images/US-NationalParkService-Logo.svg';
 const NoMatch = () => {
   return (
     <div className="NoMatch"> 
-      <h2>Sorry, this page doesn't exist.</h2>
-      <h3>Return to the{<NavLink to="/" className="NavText">Welcome Center</NavLink>}</h3>
-      <img src={Arrowhead} alt="National Parks Service arrowhead logo" />
+      <h2>Sorry, you've gone off trail.</h2>
+      <h3>Return to the {<NavLink to="/" className="NoMatch-NavText">Welcome Center</NavLink>}</h3>
+      <img src={Arrowhead} alt="National Parks Service arrowhead logo" className="NoMatch=arrowhead"/>
     </div>
   )
 }
